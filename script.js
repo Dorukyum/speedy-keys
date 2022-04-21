f50527b03ae85680d71b299af434b8f8e202888a
Dorukyum <doruk.ak@hotmail.com>
2022-04-21 22:26:01 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const $ = id => document.getElementById(id);
+
+const timeElement = $("time");
+const keyElement = $("key");
+const inputElement = $("in");
+const startButton = $("start");
+const sensitivityCheck = $("sensitivity");
+const numbersCheck = $("numbers");
+const countInput = $("count");
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function fix(number) {
+    return number.toFixed(2);
+}
+
+function clean() {
+    timeElement.innerHTML = "0.00s";
+    keyElement.innerHTML = "?";
+    inputElement.value = "";
+};
+
+async function play() {
+    const numbers = numbersCheck.checked;
+    const caseSensitive = sensitivityCheck.checked;
+    let count = parseInt(countInput.value);
+    if (count <= 0) { return alert(`Character count should be bigger than zero.`); }
+
+    clean();
+    inputElement.focus();
+    startButton.disabled = true;
+
+    let time = 0.00;
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+    if (caseSensitive) { chars += chars.toLowerCase(); }
+    if (numbers) { chars += "0123456789"; }
+    chars = chars.split("");
+
+    for (let i = 0; i < count; i++) {
+        do {
+            var char = chars[Math.floor(Math.random() * chars.length)];
+        } while (keyElement.innerHTML === char) // don't display the same char twice in a row (it's confusing)
+        keyElement.innerHTML = char;
+
+        let convert = caseSensitive ? (s => s) : (s => s.toLowerCase());
+        while (convert(char) != convert(inputElement.value.slice(i))) {
+            await sleep(0.01);
+            time += 0.01;
+            time = Math.round(time * 100) / 100;
+            timeElement.innerHTML = `${time}s`;
+        }
+    }
+
+    await sleep(0.01); // wait for the displays
+    alert(`Total time: ${time}s\n${fix(time / count)} seconds per letter\n${fix(count / time)} letters per second`);
+    startButton.disabled = false;
+    startButton.focus();
+}
